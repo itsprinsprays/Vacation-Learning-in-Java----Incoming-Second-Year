e7c6b0055e226fed3daad38dbfbda1c2a3e9edfe
itsprinsprays <prncbntz@gmail.com>
2025-07-31 18:30:14 +0800

    Added Day21

---
+++ Day21_PredicateFactoryMethod1/factoryMethods.java
@@ -0,0 +1,44 @@
+package Day21_PredicateFactoryMethod1;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class factoryMethods {
+	
+	public static Predicate<String> isUppercase(String sentence) {
+		return word -> word.equals(sentence.toUpperCase()); 
+	}
+	
+	public static Predicate<String> startsWith() {
+		return word -> {
+			if(word == null || word.isEmpty()) return false;
+			char first = Character.toUpperCase(word.charAt(0));
+			return first == 'A' || first == 'E' || first == 'I' || first == 'O' || first == 'U';
+		};
+	}
+	
+	public static Predicate<String> endsWithDigits() {
+		return word -> {
+			 if (word == null || word.isEmpty()) return false;
+			char dig = word.charAt(word.length() - 1);
+			return Character.isDigit(dig);
+			
+		};
+		
+	}	
+	
+	public static void checkword(List<String> words, Predicate<String> ConditionIs, Predicate<String> ConditionStarts, Predicate<String> ConditionEnds) {
+		
+		for(String word : words) {
+			if(ConditionIs.and(ConditionStarts).and(ConditionEnds).test(word)) {
+				System.out.println("Checking : " + word);
+				System.out.println("Qualified");
+				System.out.println("");
+			} else {
+				System.out.println("Checking : " + word);
+				System.out.println("Not Qualified");
+				System.out.println("");
+			}
+		}
+	}
+}
---
+++ Day21_PredicateFactoryMethod1/main.java
@@ -0,0 +1,22 @@
+package Day21_PredicateFactoryMethod1;
+
+import java.util.List;
+import java.util.function.Predicate;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> words = List.of("APPLE", "ORANGE1", "BANANA", "AVOCADO", "EGGPLANT7", "ICE");
+		
+		Predicate<String> isUppercase = factoryMethods.startsWith();
+		
+		Predicate<String>  startsWith = factoryMethods.startsWith();
+		
+		Predicate<String>  endsWithDigits = factoryMethods.endsWithDigits();
+		
+		factoryMethods.checkword(words, isUppercase, startsWith, endsWithDigits);
+		
+
+	}
+
+}
