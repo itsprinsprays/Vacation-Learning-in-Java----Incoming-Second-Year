265ccf70519995d9033a5a3dcdfe772637aa4731
itsprinsprays <prncbntz@gmail.com>
2025-08-02 16:47:17 +0800

    Journal Added

--- Day4_Constructors/StudentObjectSimulation.java
+++ Day4_Constructors/StudentObjectSimulation.java
@@ -11,29 +11,26 @@ public class StudentObjectSimulation {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		String firstname = null,lastname = null,course = null,section = null,year = null;
-		double midtermGrade = 0,finalGrade = 0;
-		
 		print.p("Enter Firstname : ");
-		firstname = scan.nextLine();
+		String firstname = scan.nextLine();
 		
-		print.p("Enter Lastname : ");
-		lastname = scan.nextLine();
+		print.p("Enter Lastname  : ");
+		String lastname = scan.nextLine();
 		
-		print.p("Enter Program : ");
-		course = scan.nextLine();
+		print.p("Enter Program   : ");
+		String course = scan.nextLine();
 		
-		print.p("Enter Year : ");
-		year = scan.nextLine();
+		print.p("Enter Year      : ");
+		String year = scan.nextLine();
 		
-		print.p("Enter Section : ");
-		section = scan.nextLine();
+		print.p("Enter Section   : ");
+		String section = scan.nextLine();
 		
 		print.p("Enter Midterm Grade : ");
-		midtermGrade = scan.nextInt();
+		int midtermGrade = scan.nextInt();
 		
 		print.p("Enter Final Grade : ");
-		finalGrade = scan.nextInt();
+		int finalGrade = scan.nextInt();
 		
 		print.pl("------------ INFORMATION ------------");
 		
