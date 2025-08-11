7157d713e54c643b334d0de08c5c2b5bb9240906
itsprinsprays <prncbntz@gmail.com>
2025-08-11 17:46:04 +0800

    Added Day30

---
+++ Day30_Function/main.java
@@ -0,0 +1,56 @@
+package Day30_Function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class main {
+	
+    public static void main(String[] args) {
+    	
+    	Function<String, String> addExclamation = s -> s + "!";
+    	Function<String, String> makeStars = s -> "***" + s + "***";
+    	
+    	Function<String, String> combined = addExclamation.andThen(makeStars);
+    	Function<String, String> combined1 = addExclamation.compose(makeStars);
+    	System.out.println(combined.apply("world"));  
+    	System.out.println(combined1.apply("world"));
+    	
+    	System.out.println();
+    	
+    	Function<Integer, Integer> doubleIt = s -> s*2;	
+    	Function<Integer, Integer> squareIt = s -> s*s;
+   
+    	Function<Integer, String> addLabel = number ->   "Result : " + number ;
+    	
+    	Function<Integer, String> andThen = doubleIt.andThen(squareIt).andThen(addLabel);
+    	Function<Integer, String> compose = doubleIt.compose(squareIt).andThen(addLabel);
+
+    	System.out.println(andThen.apply(5));
+    	System.out.println(compose.apply(5));
+    	
+    	
+    	System.out.println();
+    	
+    	Function<Integer, Double> NumberToHalf = s -> s * .5;
+    	Function<Double, String> DoubleToMoneyString = s -> "Money : " + s;
+    	Function<String, Integer> CountCharacters = s ->  s.length();
+    	
+    	Function<Integer, Integer> ChainInteger = NumberToHalf.andThen(DoubleToMoneyString).andThen(CountCharacters);
+    
+    	
+    	System.out.println(ChainInteger.apply(200));
+    	
+    	 List<String> names = Arrays.asList("John", "Jane", "Jack", "Anna");
+
+         names.stream()                              // 1. Source
+              .filter(name -> name.startsWith("J")) // 2. Intermediate
+              .map(String::toUpperCase)             // 2. Intermediate
+              .forEach(System.out::println);
+
+    	
+    	
+    	
+    	
+    }
+}
